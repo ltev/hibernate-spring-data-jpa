62e647c1e3dd5a2f9490ffd13b87b693a913de09
ltev <ltev@interia.pl>
2024-12-19 18:36:16 +0100

    Init BookdDBMS project

---
+++ 01-book-bd/bookdbms/src/test/java/com/ltev/bookdbms/BookdbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltev.bookdbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookdbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
