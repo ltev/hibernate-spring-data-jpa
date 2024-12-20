092ee2a7a611411d3152ebd43640af51976e67b4
ltev <ltev@interia.pl>
2024-12-20 17:12:32 +0100

    Added project for Flyway

--- 01-book-bd/bookdbms/src/main/java/com/ltev/bookdbms/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ltev.bookdbms.repository;
-
-import com.ltev.bookdbms.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-
-}
--- 01-book-bd/bookdbms/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists book;
-
-create table book (
-                      id bigint not null auto_increment,
-                      isbn varchar(255),
-                      publisher varchar(255),
-                      title varchar(255),
-                      primary key (id)
-) engine=InnoDB;
