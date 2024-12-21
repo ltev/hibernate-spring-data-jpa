aee432dbdb917c6e2ed8065af1f616d6211a46a4
ltev <ltev@interia.pl>
2024-12-21 20:01:37 +0100

    Book.authorId fk relationship added to db

--- 05-book-db-jdbc/bookdb/src/test/java/com/ltev/bookdb/domain/repository/BookRepositoryTest.java
+++ 05-book-db-jdbc/bookdb/src/test/java/com/ltev/bookdb/domain/repository/BookRepositoryTest.java
@@ -1,12 +1,15 @@
 package com.ltev.bookdb.domain.repository;
 
+import com.ltev.bookdb.domain.Author;
 import com.ltev.bookdb.domain.Book;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -15,10 +18,26 @@ class BookRepositoryTest {
     @Autowired
     private BookRepository bookRepository;
 
+    Book BOOK;
+
+    @BeforeEach
+    void setUp() {
+        BOOK = new Book("Book 1", "Author 1", "ISBN 1");
+    }
+
     @Test
     void save() {
-        Book book = bookRepository.save(new Book("Book 1", "Author 1", "ISBN 1"));
+        bookRepository.save(BOOK);
 
-        assertThat(book.getId()).isNotNull();
+        assertThat(BOOK.getId()).isNotNull();
+    }
+
+    @Test
+    void authorIdRelationship() {
+        Long nonExistingAuthorId = 1L;
+        BOOK.setAuthorId(nonExistingAuthorId);
+
+        assertThatExceptionOfType(DataIntegrityViolationException.class).isThrownBy(
+                () -> bookRepository.save(BOOK));
     }
 }
