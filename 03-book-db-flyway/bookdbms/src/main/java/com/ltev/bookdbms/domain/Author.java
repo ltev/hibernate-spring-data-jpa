6017455b00ed4fdd0cecfb212039a4ca9722bbfb
ltev <ltev@interia.pl>
2024-12-21 10:37:22 +0100

    Added Author domain an author schema to flyway directory

---
+++ 03-book-db-flyway/bookdbms/src/main/java/com/ltev/bookdbms/domain/Author.java
@@ -0,0 +1,23 @@
+package com.ltev.bookdbms.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)     // AUTO - additional table 'book_seq'
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+}
