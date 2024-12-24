8dba7fcabe0fe286579360103aebe8fcaec4ab79
ltev <ltev@interia.pl>
2024-12-24 09:49:46 +0100

    Added JPA Repositories

---
+++ 08-book-db-spring-data-jpa/bookdb/src/test/java/com/ltev/bookdb/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.ltev.bookdb.repository;
+
+import com.ltev.bookdb.domain.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
