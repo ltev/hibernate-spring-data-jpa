02500c214135501f0cf992d326668ac6a25baa1f
ltev <ltev@interia.pl>
2024-12-27 16:29:33 +0100

    New Project Order-Service

---
+++ 10-order-service/order-service/src/main/java/com/ltev/orderservice/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ltev.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
